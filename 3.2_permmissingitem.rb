9b83e9c194b6e25933ddc759f898a717b20c705b
Patrick <bishop@splatterworld.de>
2020-09-03 12:28:19 +0200

    3.2 score 100%

--- 3.2_permmissingitem.rb
+++ 3.2_permmissingitem.rb
@@ -3,11 +3,11 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+# score 100%
 
-MAX_RANGE = 100000
-MIN_RANGE = 0
-
-# An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+# An array A consisting of N different integers is given. The array contains integers
+# in the range [1..(N + 1)],
+# which means that exactly one element is missing.
 
 # Your goal is to find that missing element.
 
@@ -31,38 +31,36 @@ MIN_RANGE = 0
 # each element of array A is an integer within the range [1..(N + 1)].
 
 
+MAX_RANGE = 100000
+MIN_RANGE = 0
+
 def solution(a)
-    """
-    Find the missing element in a given permutation
-    :param A: list of integers
-    :return: the integer that is missing in O(n) time and O(1) space complexity
-    """
     raise ArgumentError.new("a needs to be array") unless a.is_a? Array
 
     # An empty list so the missing element must be "1"
     return 1 if a.size == 0
+    a_len_sum = 0
+    (1..a.length+1).each {|e| a_len_sum += e}
 
-    ## missing element in the array
-    #a.sort!
-    last = nil
-    #puts "a = #{a}"
-    #puts "a.sort = #{a.sort}"
-    a.sort.each do |elem|
-    	#puts "elem is #{elem}"
-    	if  !last.nil? &&  last +1  == elem
-    		last = elem
-    	elsif last.nil?
-    			last = elem
-    	else # break in the permutation
-    		return last +1
-    	end
-    end # each
+    a_sum = 0
+    a.each do |e|
+      raise ArgumentError.new("elements must be integers was #{e}") if !e.is_a? Integer or e < 1 or e > a.length+1
+      a_sum += e
+    end
 
-    return a.sort.last+1
+    return a_len_sum - a_sum
 end
 
-
+solution([1,3])
 assert_raise(ArgumentError.new("a needs to be array")) {solution(0)}
+assert_raise(ArgumentError.new("elements must be integers was #{1.2}")) {solution([1.2])}
+assert_raise(ArgumentError.new("elements must be integers was #{0}")) {solution([0,1])}
+assert_equal(2,solution([1,5,3,4]))
+assert_equal(5,solution([1,2,3,4]))
+
+arr = Array.new(MAX_RANGE) { |i| i+1 }
+arr.delete_at(MAX_RANGE-10)
+puts solution(arr)
 puts solution([])
-puts "===> #{solution([1,5,3,4])}"
-puts "===> #{solution([1,2,3,4])}"
+puts solution([1,5,3,4])
+puts solution([1,2,3,4])
