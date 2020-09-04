e33f2bf370818225b4f2b012fb4ae0fe54667f95
Patrick <bishop@splatterworld.de>
2020-09-04 13:07:18 +0200

    4.4 score 100%

--- 4.4_perm_check.rb
+++ 4.4_perm_check.rb
@@ -3,6 +3,8 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+# score 100% with N_MAX = 1.000.000.000
+
 # A non-empty array A consisting of N integers is given.
 # A permutation is a sequence containing each element from 1 to N once, and only once.
 # For example, array A such that:
@@ -41,24 +43,26 @@ include Test::Unit::Assertions
 # each element of array A is an integer within the range [1..1,000,000,000].
 
 N_MIN = 1
-N_MAX = 100000000
+N_MAX = 1000000000
 A_LEN_MAX = 100000
 
 def solution a
 	raise ArgumentError.new("a has to be a non empty array") if !a.is_a? Array or a.empty?
 	raise ArgumentError.new("a has to be smaller than #{A_LEN_MAX} elements") if a.length > A_LEN_MAX
-	a_sort = a.sort
-	raise ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}") if a_sort.first < N_MIN or a_sort.last > N_MAX
+	res = Hash.new
 
-	last_elem = nil
-	a_sort.each do |elem|
-		if last_elem.nil?
-			last_elem = elem
-		elsif last_elem +1 == elem
-			last_elem = elem
-		else
-			return 0
-		end
+	#fill the hash
+	a.each do |elem|
+		raise ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}, was #{elem}") if elem < N_MIN or elem > N_MAX
+		return 0 if res[elem]
+		res[elem] = elem
+	end
+
+	#return 0 if res.length != a.length
+
+	(1..a.length).each do |i|
+		next if res[i]
+		return 0
 	end
 
 	return 1
@@ -69,9 +73,15 @@ assert_raise(ArgumentError.new("a has to be a non empty array")) {solution(1)}
 assert_raise(ArgumentError.new("a has to be a non empty array")) {solution([])}
 arr = Array.new(A_LEN_MAX+1){ |i| i+1  }
 assert_raise(ArgumentError.new("a has to be smaller than #{A_LEN_MAX} elements")) {solution(arr)}
-assert_raise(ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}")) {solution([1,N_MIN-1])}
-assert_raise(ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}")) {solution([1,N_MAX+1,2000])}
+assert_raise(ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}, was #{N_MIN-1}")) {solution([1,N_MIN-1])}
+assert_raise(ArgumentError.new("a values have to be between #{N_MIN} and #{N_MAX}, was #{N_MAX+1}")) {solution([1,N_MAX+1,2000])}
 
+assert_equal(0,solution([4,1,N_MAX]))
+assert_equal(0,solution([4,3,N_MIN]))
+assert_equal(1,solution([4,1,3,2]))
+assert_equal(0,solution([4,1,3]))
+assert_equal(0,solution([4,1,3,34534,345]))
+assert_equal(0,solution([2,3,4]))
 puts solution([4,1,3,2])
 puts solution([4,1,3])
 puts solution([4,1,3,1,2])
