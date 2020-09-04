7d7c8a922164eaca03fe26a15ed5716c2c07d8e0
Patrick <bishop@splatterworld.de>
2020-09-04 13:35:08 +0200

    5.1 score 23% . crap

--- 5.1_count_div.rb
+++ 5.1_count_div.rb
@@ -3,6 +3,9 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+
+# score 23 %  - this is crap +
+
 # Write a function:
 
 # def solution(a, b, k)
@@ -25,9 +28,15 @@ K_MIN_VAL = 1
 def solution(a,b,k)
 	raise ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") if a < MIN_VAL or a > MAX_VAL
 	raise ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") if b < MIN_VAL or b > MAX_VAL
+	raise ArgumentError.new("a <= b") if a > b
 	raise ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}") if k < K_MIN_VAL or k > MAX_VAL
 
-	first_divider = nil
+
+
+
+
+	first_divider = 0
+	a = 1 if a < 1
 	(a..b).each do |elem|
 		if elem%k == 0
 			first_divider = elem
@@ -35,16 +44,26 @@ def solution(a,b,k)
 		end
 	end
 
-	puts "first_divider = #{first_divider}, b = #{b}, step = #{k}"
+	puts "first_divider: #{first_divider}"
+
 	counter = 0
-	(first_divider..b).step(k).each do |e|
-		counter = counter +1
+	if first_divider > 0
+		#puts "first_divider = #{first_divider}, b = #{b}, step = #{k}"
+		(first_divider..b).step(k).each do |e|
+			counter = counter +1
+		end
 	end
 
 	return counter
 end
 
+# puts solution(0,1,11)
+# puts solution(10,10,5)
+# puts solution(10,10,7)
+# puts solution(10,10,20)
 
+
+#A = 10, B = 10, K in {5,7,20}
 
 # assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MIN_VAL-1, 1, 1)}
 # assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MAX_VAL+1, 1, 1)}
@@ -52,7 +71,11 @@ end
 # assert_raise(ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(1, MAX_VAL+1,  1)}
 # assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,K_MIN_VAL-1)}
 # assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,MAX_VAL+1)}
-
-#puts solution(1,1,1)
-puts solution(1,14,3)
 puts solution(6,11,2)
+puts solution(MIN_VAL,MAX_VAL,127)
+
+# assert_equal(3,solution(6,11,2))
+#assert_equal(15748032,solution(MIN_VAL,MAX_VAL,127))
+# assert_equal(1, solution(1,1,1))
+# assert_equal(4, solution(1,14,3))
+#puts solution(6,11,2)
