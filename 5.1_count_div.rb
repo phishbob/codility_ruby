e51d6a87ade4336bddb9872fd952644f79cb439e
Patrick <bishop@splatterworld.de>
2020-09-09 12:17:42 +0200

    5.1 score 100%

--- 5.1_count_div.rb
+++ 5.1_count_div.rb
@@ -4,12 +4,14 @@ require "test/unit/assertions"
 include Test::Unit::Assertions
 
 
-# score 23 %  - this is crap +
+# score 100%
+
 
 # Write a function:
 
 # def solution(a, b, k)
-# that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+# that, given three integers A, B and K, returns the number of integers within the range [A..B] that are
+# divisible by K, i.e.:
 
 # { i : A ≤ i ≤ B, i mod K = 0 }
 # For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within
@@ -31,51 +33,29 @@ def solution(a,b,k)
 	raise ArgumentError.new("a <= b") if a > b
 	raise ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}") if k < K_MIN_VAL or k > MAX_VAL
 
+	#	puts "a -> #{a}, b -> #{b}, k -> #{k}"
 
-
-
-
-	first_divider = 0
-	a = 1 if a < 1
-	(a..b).each do |elem|
-		if elem%k == 0
-			first_divider = elem
-			break
-		end
-	end
-
-	puts "first_divider: #{first_divider}"
-
-	counter = 0
-	if first_divider > 0
-		#puts "first_divider = #{first_divider}, b = #{b}, step = #{k}"
-		(first_divider..b).step(k).each do |e|
-			counter = counter +1
-		end
-	end
-
-	return counter
+	res = b/k - (a-1)/k
+	#puts "all: #{b/k}, sub: #{(a-1)/k}, res : #{res}"
+	return res
 end
 
-# puts solution(0,1,11)
-# puts solution(10,10,5)
-# puts solution(10,10,7)
-# puts solution(10,10,20)
+#assert_equal(0, solution(0,1,11))
+assert_equal(1, solution(0,0,11))
+assert_equal(1, solution(10,10,5))
+assert_equal(0,solution(10,10,7))
+assert_equal(0,solution(10,10,20))
+assert_equal(7874016,solution(MAX_VAL/2,MAX_VAL,127))
+assert_equal(3,solution(6,11,2))
+assert_equal(15748032,solution(MIN_VAL,MAX_VAL,127))
+assert_equal(1, solution(1,1,1))
+assert_equal(0, solution(1,1,11))
+assert_equal(4, solution(1,14,3))
 
 
-#A = 10, B = 10, K in {5,7,20}
-
-# assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MIN_VAL-1, 1, 1)}
-# assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MAX_VAL+1, 1, 1)}
-# assert_raise(ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(1, MIN_VAL-1,  1)}
-# assert_raise(ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(1, MAX_VAL+1,  1)}
-# assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,K_MIN_VAL-1)}
-# assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,MAX_VAL+1)}
-puts solution(6,11,2)
-puts solution(MIN_VAL,MAX_VAL,127)
-
-# assert_equal(3,solution(6,11,2))
-#assert_equal(15748032,solution(MIN_VAL,MAX_VAL,127))
-# assert_equal(1, solution(1,1,1))
-# assert_equal(4, solution(1,14,3))
-#puts solution(6,11,2)
+assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MIN_VAL-1, 1, 1)}
+assert_raise(ArgumentError.new("a has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(MAX_VAL+1, 1, 1)}
+assert_raise(ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(1, MIN_VAL-1,  1)}
+assert_raise(ArgumentError.new("b has to be between #{MIN_VAL} and #{MAX_VAL}") ) {solution(1, MAX_VAL+1,  1)}
+assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,K_MIN_VAL-1)}
+assert_raise(ArgumentError.new("k has to be between #{K_MIN_VAL} and #{MAX_VAL}")) {solution(1,1,MAX_VAL+1)}
