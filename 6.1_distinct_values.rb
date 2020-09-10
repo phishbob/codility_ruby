b96fc62b1d5def26166fd69df7ea43b4c6c59825
Patrick <bishop@splatterworld.de>
2020-09-10 12:43:14 +0200

    6.1 100%, 6.2 88%

--- 6.1_distinct_values.rb
+++ 6.1_distinct_values.rb
@@ -3,6 +3,7 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+# score 100%
 
 # Write a function
 
@@ -29,28 +30,28 @@ MAX_VAL = 1000000
 
 def solution(a)
 	raise ArgumentError.new("a must be array") if !a.is_a? Array
-	raise ArgumentError.new("a length must be betwee #{A_MIN} and #{A_MAX}") if a.length > A_MAX
+	raise ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}") if a.length > A_MAX
+	hash = Hash.new
 	a.each do |elem|
 		if !elem.is_a? Integer or elem < MIN_VAL or elem > MAX_VAL
 			raise ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")
 		end
-	end
-	hash = Hash.new
-	a.each do |elem|
 		hash[elem] = true
 	end
 
 	return hash.length
 end
 
-puts solution([1,2,3])
-puts solution([1,2,0,2])
-puts solution([])
-puts solution [-2,-1,1,2,2,-2]
+assert_equal(3, solution([1,2,3]))
+assert_equal(3, solution([1,2,0,2]))
+assert_equal(0, solution([]))
+assert_equal(4, solution([-2,-1,1,2,2,-2]))
+
+arr = Array.new(A_MAX) {|i| i }
+assert_equal(A_MAX, solution(arr))
 assert_raise(ArgumentError.new("a must be array")) {solution(1)}
-arr = Array.new
-(0..A_MAX).each {|val| arr.push(1)}
-assert_raise(ArgumentError.new("a length must be betwee #{A_MIN} and #{A_MAX}")){solution(arr)}
+arr = Array.new(A_MAX+1,1)
+assert_raise(ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}")){solution(arr)}
 assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,2,'a',3])}
 assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MIN_VAL-1,2])}
 assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MAX_VAL+1,2])}
--- 6.2_max_product_of_3.rb
+++ 6.2_max_product_of_3.rb
@@ -3,6 +3,7 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+# score 88% - my solution fucks up with longer arrays with only negative numbers
 
 # A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
 
@@ -51,21 +52,46 @@ MAX_VAL = 1000
 def solution(a)
 	raise ArgumentError.new("a must be array") if !a.is_a? Array
 	raise ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}") if a.length < A_MIN or a.length > A_MAX
+
+	a_min = Array.new # values below zero
+	a_max = Array.new # values above zero
+
 	a.each do |elem|
 		if !elem.is_a? Integer or elem < MIN_VAL or elem > MAX_VAL
 			raise ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")
 		end
+
+		if elem >= 0
+			if a_max.length < 3
+				a_max.push(elem)
+			elsif a_max.any? {|e| e < elem} # we have at least one smallert item in our array
+				a_max.sort!.shift # pop smallest element
+				a.push(elem)
+			end
+		elsif elem < 0
+			if a_min.length < 3
+				a_min.push(elem)
+			elsif a_min.any? {|e| e > elem} # we have at least one bigger item in our array -  values < 0
+				a_min.sort!.pop # pop biggest element
+				a.push(elem)
+			end
+
+		end
 	end
 
+	a_result = a_max + a_min
 	max_val = nil
-	a[0..-3].each_with_index do |val1,idx1|
-		# get second element
-		a[0..-2].each_with_index do |val2, idx2|
-			next if idx2 <= idx1
-			a[0..-1].each_with_index do |val3, idx3|
-				next if idx3 <= idx2
-				mul = val1*val2*val3
-				max_val = mul if max_val.nil? or mul > max_val
+	a_result[0..-3].each_index do |i|
+		a_result[0..-2].each_index do |i2|
+			next if i2 <= i
+			tmp = a_result[i]*a_result[i2]
+			a_result[0..-1].each_index do |i3|
+				next if i3 <= i2
+				res = tmp * a_result[i3]
+				if max_val.nil? or res > max_val
+					max_val = res
+					puts "#{a_result[i]} * #{a_result[i2]} * #{a_result[3]} = #{max_val}"
+				end
 			end
 		end
 	end
@@ -73,13 +99,33 @@ def solution(a)
 	return max_val
 end
 
-puts solution([1,2,3,1,-3,-4,4])
+puts solution([-5, -6, -4, -7, -10])
 
-assert_raise(ArgumentError.new("a must be array")) {solution(1)}
-arr = Array.new
-(0..A_MAX).each {|val| arr.push(1)}
-assert_raise(ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}")){solution([1,2])}
-assert_raise(ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}")){solution(arr)}
-assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,2,'a',3])}
-assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MIN_VAL-1,2])}
-assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MAX_VAL+1,2])}
+# assert_equal(6, solution([1,2,3]))
+# assert_equal(0, solution([1,2,0]))
+# assert_equal(-80, solution([-10,-2,-4]))
+# assert_equal(120, solution([-4, -6, 3, 4, 5]))
+
+# arr = Array.new(100000) { rand(-2..1)  }
+# assert_equal(4, solution(arr))
+
+# arr = Array.new(100000) { rand(0..950)  }
+# arr[100] = 1000
+# arr[10000] = 1000
+# arr[99999] = 1000
+# assert_equal(1000000000, solution(arr))
+# assert_equal(0,solution([0,0,0,0]))
+# assert_equal(0, solution([1,2,0,0]))
+
+# puts solution([1,3,-1])
+
+#assert_equal(48,solution([1,2,3,1,-3,-4,4]))
+
+# assert_raise(ArgumentError.new("a must be array")) {solution(1)}
+# arr = Array.new
+# (0..A_MAX).each {|val| arr.push(1)}
+# assert_raise(ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}")){solution([1,2])}
+# assert_raise(ArgumentError.new("a length must be between #{A_MIN} and #{A_MAX}")){solution(arr)}
+# assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,2,'a',3])}
+# assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MIN_VAL-1,2])}
+# assert_raise(ArgumentError.new("a elements have to be integers between #{MIN_VAL}..#{MAX_VAL}")){solution([1,MAX_VAL+1,2])}
