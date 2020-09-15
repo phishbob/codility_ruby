27f42d22a583d520637beb03c0282ad631315d18
Patrick <bishop@splatterworld.de>
2020-09-15 10:58:20 +0200

    7.1 score 100%

--- 7.1_brackets.rb
+++ 7.1_brackets.rb
@@ -3,6 +3,8 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+# score 100%
+
 # A string S consisting of N characters is considered to be properly nested if any of the following conditions
 # is true:
 
@@ -24,41 +26,46 @@ include Test::Unit::Assertions
 # N is an integer within the range [0..200,000];
 # string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
 
+MAX_LENGTH = 200000
 
 def solution(s)
-	puts "--------------------"
-	return true if s.length == 0
-	my_string = "()[]{}"
-	s.each_char do |c|
-		raise ArgumentError.new("s may only consist of (){}[]") unless my_string.include? c
-	end
-	puts "string = #{s}"
-	my_hash = {"(" => 0, "[" => 0, "{" => 0}
-	vals = {")" => "(", "]" => "[", "}" => "{"}
+	raise ArgumentError.new("s has to be string with max length of #{MAX_LENGTH}") if !s.is_a? String or s.length > MAX_LENGTH
+	return 0 if s.length%2 > 0
 
-	nested = Array.new
+#	puts "s = #{s}"
+
+	vals = {")" => "(", "]" => "[", "}" => "{"}
+	res = []
 	s.each_char do |c|
-		if "([{".include? c
-			nested.push(c)
-		else # closing bracket
-			if nested.last == vals[c]
-				nested.pop
+	#	puts "c: #{c} || res: #{res.inspect}"
+		if "[({".include? c
+			res.push c
+		elsif "]})".include? c
+			if res.last == vals[c]
+				res.pop
 			else
-				return false
+				#puts "meh. #{c}, res: #{res.inspect}"
+				return 0
 			end
+		else
+			raise ArgumentError.new("only following characters allowed: '[](){}' ") unless '[](){}'.include? c
 		end
 	end
 
-	return false unless nested.empty?
-	return true
+	#puts res.inspect
+	return 0 if res.length > 0
+	return 1
 end
 
-assert_raise(ArgumentError.new("s may only consist of (){}[]")) {solution("a")}
+assert_equal(0, solution("()()()())")) # 0 - uneven number of brackets
+assert_equal(1, solution("()()()()")) # 1
+assert_equal(1, solution("([((((()))))])")) #1
+assert_equal(0, solution("([((((())))]))")) # 0
 
-puts solution("")
-puts solution("(")
-puts solution("()")
-puts solution ("{[()()]}")
-puts solution ("([)()]")
-puts solution("()(")
-puts solution("()}")
+#assert_equal(1, solution("{[()()]}")) #1
+#assert_equal(0, solution("([)()]")) #0
+assert_raise( ArgumentError.new("only following characters allowed: '[](){}' ")){solution("([((((()))))b]")}
+
+# assert_raise( ArgumentError.new("s has to be string with max length of #{MAX_LENGTH}")) {solution(12)}
+# s = "a"*(MAX_LENGTH+1)
+# assert_raise( ArgumentError.new("s has to be string with max length of #{MAX_LENGTH}")) {solution(s)}
