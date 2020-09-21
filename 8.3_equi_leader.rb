06d8a56cd2170e1c326d71d19c5c34041c555269
Patrick <bishop@splatterworld.de>
2020-09-21 14:33:39 +0200

    8.2 score 100%

--- 8.3_equi_leader.rb
+++ 8.3_equi_leader.rb
@@ -3,7 +3,7 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
-# score 66% correctness 100% - performance 25%
+# score 100%
 # A non-empty array A consisting of N integers is given.
 
 # The leader of this array is the value that occurs in more than half of the elements of A.
@@ -69,50 +69,41 @@ def solution(a)
 	end
 
 	my_h_sort = my_h.sort_by {|k, v| -v}
-	#puts "my_h_sort: #{my_h_sort[0][0]}, #{my_h_sort[1]}"
 	# -> my_h_sort[value][occurances]
-	if my_h_sort.first[1] < a.size/2
-		return 0
-	end
+	return 0 if my_h_sort.first[1] < a.size/2
 	leader_val = my_h_sort.first[0]
-	#puts "leader_val: #{leader_val}"
+
+	occ_array = Array.new
+	occurances = 0
+	a.each do |e|
+		occurances += 1 if e == leader_val
+		occ_array.push(occurances)
+	end
+	#puts occ_array.inspect
 
 	for idx in (0...a.length-1) do
-	#for idx in (0...1000) do
-		leader = false
-		leader2 = false
-		count_low = 0
+		sum1 = occ_array[idx]
+		sum2 = occ_array.last - occ_array[idx]
 
-		(0..idx).each do |i|
-			count_low += 1 if a[i] == leader_val
+		# puts "#{idx}+1  < #{sum1*2}"
+		# puts "#{(a.length - idx -1)}  < #{(sum2*2 )} "
+
+		if (idx+1)  < sum1 * 2 &&  (a.length - idx - 1 )   < sum2 * 2
+			## we have a leader
+			#puts "YEAH #{idx}"
+			ret += 1
 		end
-
-		leader = true if count_low > ((idx+1)/2.0)
-		#puts "leader: #{leader}, count_low: #{count_low}, treshold = #{(idx+1)/2.0}"
-		next unless leader
-		count_high = 0
-		(idx+1...a.length).each do |i|
-			count_high += 1 if a[i] == leader_val
-		end
-
-		leader2 = true if count_high > (a.length- idx-1 )/2.0
-		#puts "leader2: #{leader2}, count_high: #{count_high}, treshold = #{(a.length- idx-1 )/2.0}"
-
-		ret += 1 if leader && leader2
-		puts "-------- ret: #{ret}"
+			#puts "-------- ret: #{ret}"
 	end
-##### END OF CODE
-
-
-
 	return ret
-
 end
 
 assert_equal(2, solution([4,3,4,4,4,2])) #2
 assert_equal(3, solution([1,2,1,1,1])) #3
 assert_equal(3, solution([2,1,1,1,1,1]) )
 assert_equal(0, solution([1,2,4,5]))
+assert_equal(1, solution([0,0]))
 assert_equal(0, solution([1])) #0
-arr = Array.new(1000) { rand(MIN_VAL..MAX_VAL)  }
-puts solution(arr)
+arr = Array.new((MAX_LEN/2)-1,1)
+arr2 = Array.new((MAX_LEN/2)+1,2)
+puts solution((arr+arr2).shuffle)
